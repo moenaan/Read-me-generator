215404b2516759ecac683c9df694153cef6f8ffc
Mohammed Samod <mohammedlee27@gmail.com>
2021-11-12 11:42:13 -0600

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,43 +2,63 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 
 // Questions
 const questions = [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ];
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?",
+  },
+  {
+    type: "list",
+    name: "badge",
+    message: "What license badge?",
+    choices: ['Apache','GPLv3','BSD 3-clause','MIT','GPLv2','BSD 2-clause','LGPLv3','AGPLv3'],
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is your project description",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "What is your installation process?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide usage information.",
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "Who contributed to this project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What are your test instructions?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+];
   
 //   .then((answers) => {
 //     const htmlPageContent = generateHTML(answers);
